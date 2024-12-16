3a8c2007a3ed80dd376ac5c5eed6ad8f1dd152a0
Samuel Marks <807580+SamuelMarks@users.noreply.github.com>
2024-12-15 23:02:20 -0800

    [setup.sh] Set `TARGET_OS` and `PKG_MGR`

--- setup.sh
+++ setup.sh
@@ -1,8 +1,32 @@
 #!/bin/sh
 
-export TARGET_OS='debian'
+export UNAME="$(uname)"
+case "$UNAME" in
+  'Darwin')
+    export PKG_MGR='brew'
+    TARGET_OS="$(sw_vers --productName)"
+    ;;
+  'Linux')
+    TARGET_OS="$(source /etc/os-release; printf '%s' "$ID")"
+    if [ "$TARGET_OS" = 'debian' ] \
+     || [ "$(source /etc/os-release; printf '%s' "$ID_LIKE")" = 'debian' ]; then
+       export PKG_MGR='apt'
+    else
+      >&2 printf 'Unimplemented, package manager for %s\n' "$TARGET_OS"
+      exit 3
+    fi
+    ;;
+  *)
+    >&2 printf 'Unimplemented for %s\n' "$UNAME"
+    exit 3
+    ;;
+esac
+export TARGET_OS
+
 export WWWROOT=
 export FACESWAPPER_VENV=
+
+export
 
 # If you skip this step, set env var `DATABASE_URL`
 ./postgres/"setup_${TARGET_OS}.sh"
@@ -11,6 +35,7 @@ export FACESWAPPER_VENV=
 
 ./rust/setup.sh
 ./nodejs/setup.sh
+./python/setup.sh
 
 ./nginx/"setup_${TARGET_OS}.sh"
 
