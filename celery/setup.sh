b907555f979bda6cac69d42be28c5ca0fd98789f
Samuel Marks <807580+SamuelMarks@users.noreply.github.com>
2024-12-17 21:10:47 -0800

    [celery/setup.sh] hard link rather than symbolic

--- celery/setup.sh
+++ celery/setup.sh
@@ -1,1 +1,13 @@
-/Users/samuel/repos/replica-scripts/_common/setup.sh
+#!/bin/sh
+
+if [ -n "$ZSH_VERSION" ] || [ -n "$BASH_VERSION" ]; then
+  set -euo pipefail
+fi
+
+DIR="$( dirname -- "$( readlink -nf -- "$0" )")"
+export DIR
+
+ROOT="$( dirname -- "$( dirname -- "$( readlink -nf -- "$0" )" )" )"
+export SCRIPT_ROOT_DIR="${SCRIPT_ROOT_DIR:-$ROOT}"
+
+"$SCRIPT_ROOT_DIR"'/setup_'"${TARGET_OS}"'.sh'
